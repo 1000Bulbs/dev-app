226a1c5147b97118e48cb3d32dac72f7a6e7ae8c
Michael Hoffert <mike.hoffert@gmail.com>
2013-01-23 16:34:08 -0600

    Fizzbuzz app

---
+++ 01-fizz_buzz/fizzbuzz.php
@@ -0,0 +1,39 @@
+<?php
+class FizzBuzz {
+	public function iterate($max = 100) {
+		for($i = 1; $i <= $max; $i++) {
+			$this->makeNoise($i);
+		}
+	}
+
+	public function makeNoise($i) {
+		$fizz = $this->fizz($i);
+		$buzz = $this->buzz($i);
+		if(is_null($fizz) != true) {
+			echo $fizz;
+		}
+		if(is_null($buzz) != true) {
+			echo $buzz;
+		}
+		if(is_null($fizz) && is_null($buzz)) {
+			echo $i;
+		}
+		echo "\n";
+	}
+
+	public function fizz($i) {
+		return $this->noise($i, 3, 'Fizz');
+	}
+	
+	public function buzz($i) {
+		return $this->noise($i, 5, 'Buzz');
+	}
+
+	public function noise($i, $multiple, $noise) {
+		$return = null;
+		if(($i % $multiple) == 0) {
+			$return = $noise;
+		}
+		return $return;
+	}
+}
