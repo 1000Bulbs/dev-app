fde909123d508a72a2f10a60d4b56283a602d01f
Lars Levie <larslevie@gmail.com>
2013-04-21 16:37:06 -0700

    Load frames through games

--- 01-fizz_buzz/app/controllers/frames_controller.rb
+++ 01-fizz_buzz/app/controllers/frames_controller.rb
@@ -1,5 +1,7 @@
 class FramesController < ApplicationController
-  load_and_authorize_resource through: :game
+  # load_and_authorize_resource through: :game
+  load_and_authorize_resource :game
+  load_and_authorize_resource :frame, through: :game
 
   def index
   end
