f308a546b533bb9d698e2f135bd05772e6a4ecca
Lars Levie <larslevie@gmail.com>
2013-04-21 16:20:58 -0700

    Frame migration

--- 01-fizz_buzz/db/schema.rb
+++ 01-fizz_buzz/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130421214940) do
+ActiveRecord::Schema.define(:version => 20130421225050) do
+
+  create_table "frames", :force => true do |t|
+    t.integer  "game_id"
+    t.integer  "position"
+    t.string   "fizz_buzz"
+    t.integer  "answer"
+    t.boolean  "correct"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "games", :force => true do |t|
     t.integer  "user_id"
