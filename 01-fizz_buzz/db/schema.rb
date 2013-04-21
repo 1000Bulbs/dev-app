1094f6bd7f26e47c35cab59427d58db4358d40e2
Lars Levie <larslevie@gmail.com>
2013-04-21 15:22:53 -0700

    Migrate

--- 01-fizz_buzz/db/schema.rb
+++ 01-fizz_buzz/db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(:version => 20130421214940) do
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(:version => 20130421214940) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.boolean  "admin",                  :default => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
