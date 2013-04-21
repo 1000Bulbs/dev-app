9d62704c7f026da5a71a2923fc6d6ca4e070d53c
Lars Levie <larslevie@gmail.com>
2013-04-21 14:52:36 -0700

    Remove test files for game

--- 01-fizz_buzz/app/models/game.rb
+++ 01-fizz_buzz/app/models/game.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer
+#  score          :integer
+#  time_to_finish :integer
+#  high_number    :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Game < ActiveRecord::Base
   attr_accessible :high_number, :score, :time_to_finish, :user_id
 end
--- 01-fizz_buzz/db/schema.rb
+++ 01-fizz_buzz/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130421193957) do
+ActiveRecord::Schema.define(:version => 20130421214940) do
+
+  create_table "games", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "score"
+    t.integer  "time_to_finish"
+    t.integer  "high_number"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
