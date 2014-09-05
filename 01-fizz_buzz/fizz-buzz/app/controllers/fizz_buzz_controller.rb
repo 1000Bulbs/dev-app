135db99d52befe90101b524000c07160f03c090c
James Childers <james.childers@gmail.com>
2014-09-05 14:01:49 -0500

    Finish up UI work

--- 01-fizz_buzz/fizz-buzz/app/controllers/fizz_buzz_controller.rb
+++ 01-fizz_buzz/fizz-buzz/app/controllers/fizz_buzz_controller.rb
@@ -1,30 +1,20 @@
 class FizzBuzzController < ApplicationController
   respond_to :json
 
-  def isFizz
-    calcAndRespond(3)
+  def fizzBuzz
+    fbData = []
+    for i in 1..100
+      if i % 3 == 0 && i % 5 == 0 then 
+        fbData[i] = 'FizzBuzz'
+      elsif i % 3 == 0 then 
+        fbData[i] = 'Fizz'
+      elsif i % 5 == 0 then
+        fbData[i] = 'Buzz'
+      else
+        fbData[i] = i
+      end
+    end
+    
+    respond_with(fbData.compact!)
   end
-
-  def isBuzz
-    calcAndRespond(5)
-  end
-
-  def isFizzBuzz
-    dividend = params[:val].to_i
-    fb = dividend % 3 == 0 && dividend % 5 == 0
-    @result = { "val" => dividend, "result" => fb }
-
-    respond_with(@result)
-  end
-
-  private
-
-  def calcAndRespond(divisor)
-    dividend = params[:val].to_i
-    fizz = dividend % divisor == 0
-    @result = { "val" => dividend, "result" => fizz }
-
-    respond_with(@result)
-  end
-
 end
--- 01-fizz_buzz/fizz-buzz/config/routes.rb
+++ 01-fizz_buzz/fizz-buzz/config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'static_pages/index'
 
-  get 'isFizz/:val' => 'fizz_buzz#isFizz', :defaults => { :format => 'json' }
-  get 'isBuzz/:val' => 'fizz_buzz#isBuzz', :defaults => { :format => 'json' }
-  get 'isFizzBuzz/:val' => 'fizz_buzz#isFizzBuzz', :defaults => { :format => 'json' }
+  get 'fizzBuzz' => 'fizz_buzz#fizzBuzz', :defaults => { :format => 'json' }
 end
